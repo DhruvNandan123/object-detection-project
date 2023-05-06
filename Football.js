4a9632b4d64f2618bcdc150ed2644cc134eb2846
DhruvNandan123 <91406270+DhruvNandan123@users.noreply.github.com>
2023-05-06 12:16:53 +0530

    Add files via upload

---
+++ Football.js
@@ -0,0 +1,30 @@
+Status = "";
+Football_image = "";
+
+function preload(){
+    football_image = loadImage("football.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(football_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(football_image,0,0,640,350);
+}
